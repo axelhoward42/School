7ea1f04d0c9e324e2630dd33ca10daeff0d5a749
axelhoward42 <axel.i.howard@gmail.com>
2023-10-08 16:43:03 -0600

    testing

---
+++ Project1/project1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+
+/*#1
+
+char shiftChar(char c, int shift){
+    
+    char encrypt;
+    char base = 'a',top = 'z';
+
+    if(c<base || c>top){
+        return c;
+    }
+
+    else{
+        encrypt = ((c-base+shift)%26 + 26)%26 + base;
+        return encrypt;
+    }
+
+    return encrypt;
+}
+
+int main(){
+
+    char c;
+    int shift;
+
+    cout<<"Enter a letter to encrypt:"<<endl;
+    cin>>c;
+
+    cout<<"Enter an integer to shift the letter by:"<<endl;
+    cin>>shift;
+
+    cout<<"Your encrypted letter is: "<<shiftChar(c,shift)<<endl;
+}
+*/
+
+
+int getMethod2ShiftAmount(){
+
+
+}
+
+
+
+
+int main(){
+    
+}
