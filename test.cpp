5b6684f8b4728e9785b57287cd5c2a58634368a5
axelhoward42 <axel.i.howard@gmail.com>
2023-10-08 16:42:08 -0600

    testing

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"axel"<<endl;
-    cout << "test" << endl;
+char toLower(char letter) { // If it is a Capital Letter, it will convert it to a lowercase and return it.
+    if (letter >= 65 && letter <= 90) {
+        return letter + 32; // The difference between Lowercase and Capital letters is the integer value 32.
+    }
+    return letter; // otherwise (a non-alphabet character in the ASCII table) it will return its' original
 }
-
 
 
 
